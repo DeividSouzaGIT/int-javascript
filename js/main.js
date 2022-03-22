33b9caca8bb6fcc0d1aa69ae09de0b35fae54182
DeividSouzaGIT <deividftpgit@gmail.com>
2022-03-22 00:18:44 -0300

    Array-dicionario

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-var nome = "Deivid";
-var idade = 22;
-var idade2 = 10;
-var frase = "Alemanhã melhor time do mundo!";
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"branca"}]
+console.log(frutas);
+alert(frutas[1].cor);
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var list = ["maça", "péra", "uva"];
+//list.push("laranja");
+//list.pop();
+//console.log(list.reverse());
+//console.log(list.toString());
+//console.log(list.join(" - "))
+
+
+//var nome = "Olaf Santos";
+//var n1 = 22;
+//var n2 = 10;
+//var frase = "Alemanhã melhor time do mundo!";
 //alert(nome + " tem " + idade + " anos ");
 //alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.replace("Alemanhã", "Brasil"));
-//alert(frase.replace("Alemanhã", "Brasil"))
+//console.log(nome);
+//console.log(n1 - n2);
+//console.log(frase.toUpperCase());
+//alert(frase.replace("Alemanhã", "Brasil"));

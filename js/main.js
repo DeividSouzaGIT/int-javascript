fce938ae2c74726e1c1742b0d669f0cc5af37fd8
DeividSouzaGIT <deividftpgit@gmail.com>
2022-03-21 22:40:22 -0300

    Add-js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Deivid";
+var idade = 22;
+var idade2 = 10;
+var frase = "Alemanhã melhor time do mundo!";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Alemanhã", "Brasil"));
+//alert(frase.replace("Alemanhã", "Brasil"))
